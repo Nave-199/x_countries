c8ef440c0e7b8f637c1c75a0766f8944ba50fb7a
NaVeEN98801 <naveenn199999@gmail.com>
2024-07-23 19:24:54 +0530

    completed

---
+++ src/Countries.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import "./Countries.css";
+
+const CountryCard = ({ name, flagimg, flagalttext }) => {
+  return (
+    <div className="cart">
+      <img src={flagimg} alt={flagalttext} className="image" />
+      <h2>{name}</h2>
+    </div>
+  );
+};
+
+function Countries() {
+  const Apiurl = "https://xcountries-backend.azurewebsites.net/all";
+
+  const [countries, setCountries] = useState([]);
+  console.log({ countries });
+
+  useEffect(() => {
+    fetch(Apiurl)
+      .then((res) => res.json())
+      .then((data) => {
+        setCountries(data);
+      })
+      .catch((error) => console.log("Error:", error));
+  }, []);
+
+  return (
+    <div className="main">
+      {countries.map((country) => (
+        <CountryCard
+          key={country.abbr}
+          flagimg={country.flag}
+          name={country.name}
+          flagalttext={country.flagalttext}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Countries;
